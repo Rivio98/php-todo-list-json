bade64457b54db9717fa3a78806b6e24f75f1219
Rivio98 <rivaldogjoni1998@gmail.com>
2024-09-10 10:07:28 +0200

    [FIX] Fix code

--- js/script.js
+++ js/script.js
@@ -14,11 +14,7 @@ createApp({
     methods: {
         getData() {
             axios.get(this.url).then((resp) => {
-                resp.data.forEach(todo => {
-                    console.log(`ID = ${todo.id}, Done = ${todo.done}, Name = ${todo.name}`);
-                });
-            }).catch((error) => {
-                console.error('Errore durante il recupero dei dati:', error);
+                console.log(resp.data);
             });
         }
     }
